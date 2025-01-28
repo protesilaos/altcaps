2606eafb4352a0c4a2d9f3a760ff234020772d8d
Protesilaos Stavrou <info@protesilaos.com>
2025-01-28 09:13:23 +0200

    Update to altcaps version 1.3.0

--- altcaps.el
+++ altcaps.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/altcaps
-;; Version: 1.2.0
+;; Version: 1.3.0
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This file is NOT part of GNU Emacs.
